8740dbd6e114ae7157c71f7cca5c4846ed6f8250
oussama berhili <oussamaberhili190@gmail.com>
2025-05-14 09:52:13 +0100

    Initial Commit

---
+++ lay it here/lay_it_hereApp.swift
@@ -0,0 +1,17 @@
+//
+//  lay_it_hereApp.swift
+//  lay it here
+//
+//  Created by oussama berhili on 14/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct lay_it_hereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lay it hereTests/lay_it_hereTests.swift
@@ -0,0 +1,16 @@
+//
+//  lay_it_hereTests.swift
+//  lay it hereTests
+//
+//  Created by oussama berhili on 14/5/2025.
+//
+
+import Testing
+
+struct lay_it_hereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
